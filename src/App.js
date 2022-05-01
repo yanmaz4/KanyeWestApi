64a5b8024dfcfff8b1dceafcc58302be1cbb25b5
Ercan Yanmaz <yanmaerc@gmail.com>
2022-05-01 03:07:40 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+
 function App() {
+  const API = "https://api.kanye.rest/";
+  const Solution = () => {
+    const [quote, setQuote] = useState("initial state of quote");
+
+    const getData = () =>
+        fetch(API)
+            .then((result) => result.json())
+            .then((json) => json.quote)
+            .then((quote) => setQuote(quote));
+            useEffect(() => {
+              const timer = setInterval(getData, 6000); 
+              return () => clearInterval(timer) 
+          }, []);
+      
+          return <h1>{quote}</h1>;
+      };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Solution />
     </div>
   );
 }
